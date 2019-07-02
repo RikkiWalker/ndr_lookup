bff10e86557e6039d21cdc7c2225afbea39f3f28
Rikki <rikki.walker@phe.gov.uk>
2019-07-02 12:31:24 +0100

    # Fixing the NhsdOds Organisation tests

--- test/nhsd_ods/organisation_test.rb
+++ test/nhsd_ods/organisation_test.rb
@@ -5,7 +5,7 @@ module NdrLookup
   module NhsdOds
     # The orgaisation class tests
     class OrganisationTest < Minitest::Test
-      def organisation_should_have_model_like_behaviour
+      def test_organisation_should_have_model_like_behaviour
         url  = ODS_ENDPOINT + 'organisations/X26'
         file = File.new(RESPONSES_DIR + '/nhsd_ods/organisation_find_success_response.txt')
         stub_request(:get, url).to_return(file)
@@ -14,7 +14,7 @@ module NdrLookup
         assert_equal org.name, 'NHS DIGITAL'
       end
 
-      def should_raise_error_if_not_valid_find_method
+      def test_should_raise_error_if_not_valid_find_method
         url  = ODS_ENDPOINT + 'organisations'
         file = File.new(RESPONSES_DIR + '/nhsd_ods/organisation_all_not_acceptable_response.txt')
         stub_request(:get, url).to_return(file)
